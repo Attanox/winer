0974da06804e80f7f9449f352cdf9ff357d4f882
Andrew <attanox@gmail.com>
2019-06-18 16:10:29 +0200

    Add files via upload

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -17255,6 +17255,100 @@ function (_Highway$Transition) {
 
 var _default = Fade;
 exports.default = _default;
+},{"@dogstudio/highway":"node_modules/@dogstudio/highway/build/highway.js","gsap":"node_modules/gsap/index.js","assert":"C:/Users/Andy/AppData/Roaming/npm/node_modules/parcel-bundler/node_modules/assert/assert.js"}],"js/transition_about.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _highway = _interopRequireDefault(require("@dogstudio/highway"));
+
+var _gsap = require("gsap");
+
+var _assert = require("assert");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var Fade_about =
+/*#__PURE__*/
+function (_Highway$Transition) {
+  _inherits(Fade_about, _Highway$Transition);
+
+  function Fade_about() {
+    _classCallCheck(this, Fade_about);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Fade_about).apply(this, arguments));
+  }
+
+  _createClass(Fade_about, [{
+    key: "in",
+    value: function _in(_ref) {
+      var from = _ref.from,
+          to = _ref.to,
+          done = _ref.done;
+      var my_height;
+      if (window.innerWidth < 769) my_height = document.querySelector(".slide").offsetHeight * 3;else my_height = document.querySelector(".slide").offsetHeight;
+      var tl = new _gsap.TimelineLite();
+      tl.fromTo(to, 0.5, {
+        left: '-100%',
+        top: '50%'
+      }, {
+        left: '0%'
+      }).fromTo(to, 0.5, {
+        height: '2vh',
+        width: '100%'
+      }, {
+        height: my_height.toString(),
+        top: '0vh',
+        onComplete: function onComplete() {
+          from.remove();
+          done();
+        }
+      }).fromTo(to.children[0], 2, {
+        opacity: 0
+      }, {
+        opacity: 1
+      }).fromTo(to, 0.7, {}, {
+        backgroundImage: 'none'
+      });
+      setTimeout(function () {
+        window.location.reload(false);
+      }, 1100);
+    }
+  }, {
+    key: "out",
+    value: function out(_ref2) {
+      var from = _ref2.from,
+          done = _ref2.done;
+      done();
+    }
+  }]);
+
+  return Fade_about;
+}(_highway.default.Transition);
+
+var _default = Fade_about;
+exports.default = _default;
 },{"@dogstudio/highway":"node_modules/@dogstudio/highway/build/highway.js","gsap":"node_modules/gsap/index.js","assert":"C:/Users/Andy/AppData/Roaming/npm/node_modules/parcel-bundler/node_modules/assert/assert.js"}],"js/index.js":[function(require,module,exports) {
 "use strict";
 
@@ -17262,14 +17356,21 @@ var _highway = _interopRequireDefault(require("@dogstudio/highway"));
 
 var _transition = _interopRequireDefault(require("./transition"));
 
+var _transition_about = _interopRequireDefault(require("./transition_about"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var H = new _highway.default.Core({
   transitions: {
-    default: _transition.default
+    home: _transition.default,
+    about: _transition_about.default,
+    contact: _transition.default // transitions: {
+    //     default: Fade
+    // }
+
   }
 });
-},{"@dogstudio/highway":"node_modules/@dogstudio/highway/build/highway.js","./transition":"js/transition.js"}],"C:/Users/Andy/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"@dogstudio/highway":"node_modules/@dogstudio/highway/build/highway.js","./transition":"js/transition.js","./transition_about":"js/transition_about.js"}],"C:/Users/Andy/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -17297,7 +17398,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63146" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62247" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
